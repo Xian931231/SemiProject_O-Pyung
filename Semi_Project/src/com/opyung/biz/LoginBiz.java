0ccb385643816075a1670c260516f618b4222f47
HansolKo <lavalliere125@gmail.com>
2021-07-01 21:47:15 +0900

    [update]로그인

---
+++ Semi_Project/src/com/opyung/biz/LoginBiz.java
@@ -0,0 +1,16 @@
+package com.opyung.biz;
+
+public class LoginBiz {
+/*
+	public MemberBoardDto login(String id, String pw) {
+		Connection con = getConnection();
+		
+		MemberBoardDao dao = new MemberBoardDao();
+		MemberBoardDto dto = dao.login(con,id,pw);
+		
+		close(con);
+		return dto;
+	}
+	
+	*/
+}
